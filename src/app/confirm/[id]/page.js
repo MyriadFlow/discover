9ab332e6b04159ef5c470632b6e2e916b856eddb
Devsi <grace.emily876@gmail.com>
2024-06-21 14:26:39 +0530

    success popup

--- src/app/confirm/[id]/page.js
+++ src/app/confirm/[id]/page.js
@@ -6,6 +6,7 @@ const ConfirmAddr = ({ params }) => {
   const id = params?.id;
 
   const [isHovered, setIsHovered] = useState(false);
+  const [confirmClicked, setconfirmClicked] = useState(false);
 
   return (
     <div>
@@ -106,7 +107,7 @@ const ConfirmAddr = ({ params }) => {
           </div>
 
           <div className="mt-10" style={{ display: "flex", gap: "20px" }}>
-            <Link href={`/confirm/${id}`}
+            <button
               className="justify-center flex"
               style={{
                 backgroundColor: "#30D8FF",
@@ -117,14 +118,123 @@ const ConfirmAddr = ({ params }) => {
                 borderRadius:'15px',
                 fontSize: '20px'
               }}
+              onClick={()=>{setconfirmClicked(true)}}
             >
               Confirm address
-            </Link>
+            </button>
           </div>
 
 
         </div>
       </div>
+
+
+      { confirmClicked && (
+  <div
+    style={{
+      backgroundColor: "#FFFFFFB2",
+      display: "flex",
+      overflowY: "auto",
+      overflowX: "hidden",
+      position: "fixed",
+      inset: 0,
+      zIndex: 50,
+      justifyContent: "center",
+      alignItems: "center",
+      width: "100%",
+      maxHeight: "100%",
+    }}
+    id="popupmodal"
+  >
+    <div
+      style={{
+        position: "relative",
+        padding: "16px",
+        width: "100%",
+        maxWidth: "32rem",
+        maxHeight: "100%",
+      }}
+    >
+      <div
+        style={{
+          position: "relative",
+          borderRadius: "0.5rem",
+          boxShadow: "0 0.25rem 0.75rem rgba(0, 0, 0, 0.25)",
+          color: "white",
+          background: "linear-gradient(to bottom right, #A25FF8 0%, #30D8FF 100%)",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "flex-end",
+            padding: "16px",
+            borderRadius: "20px",
+            borderColor: "#4B5563",
+          }}
+        >
+          {/* Add any additional content or buttons here */}
+        </div>
+
+        <div style={{ padding: "16px", spaceY: "16px" }}>
+          <p style={{ fontSize: "1.875rem", textAlign: "center", fontWeight: "bold"}}>
+          Address confirmed
+          </p>
+          <p style={{ fontSize: "1.2rem", textAlign: "center", paddingTop: "40px" }}>
+          Product is on the way!
+          </p>
+        </div>
+        <div style={{ display: "flex", alignItems: "center", paddingTop: "20px", paddingBottom: "16px" }}>
+          <Link
+            href=""
+            type="button"
+            style={{
+              width: "50%",
+              marginLeft: "auto",
+              marginRight: "auto",
+              color: "black",
+              fontWeight: "600",
+              focusRing: "4px",
+              outline: "none",
+              borderRadius: "30rem",
+              fontSize: "1rem",
+              padding: "10px 20px",
+              textAlign: "center",
+              backgroundColor: 'white',
+            }}
+          >
+            Experience
+          </Link>
+        </div>
+
+        <div style={{ display: "flex", alignItems: "center", paddingTop: "16px", paddingBottom: "80px" }}>
+          <Link
+            href=""
+            type="button"
+            style={{
+              width: "50%",
+              marginLeft: "auto",
+              marginRight: "auto",
+              color: "white",
+              fontWeight: "600",
+              focusRing: "4px",
+              outline: "none",
+              borderRadius: "30rem",
+              fontSize: "1rem",
+              padding: "10px 20px",
+              textAlign: "center",
+              border:'1px solid white'
+            }}
+          >
+            View my assets
+          </Link>
+        </div>
+      </div>
+    </div>
+  </div>
+)}
+
 
     
     </div>
