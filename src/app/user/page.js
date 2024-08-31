a8262f64bfb7914dbc79528f437a084b9eb4a8a3
Suraj Mahto <surajmahto255@gmail.com>
2024-08-31 15:43:43 +0530

    feat : linked user to their respective pages

--- src/app/user/page.js
+++ src/app/user/page.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import Header1 from '../../components/header1';
 import { useAccount } from "wagmi";
 import 'react-toastify/dist/ReactToastify.css';
+import Link from "next/link";
 
 function User() {
     const { address } = useAccount();
@@ -41,43 +42,44 @@ function User() {
                 <h1 className="text-2xl lg:text-3xl font-bold mb-6 lg:mb-10">All Users</h1>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 lg:gap-6">
                     {users.map((user, index) => (
-                        <div key={index} className="bg-white shadow-lg rounded-lg p-4">
-                            <div className="relative">
-                                {user.cover_image ? (
+                        <Link href={`/${user.username}`}>
+                            <div key={index} className="bg-white shadow-lg rounded-lg p-4">
+                                <div className="relative">
+                                    {user.cover_image ? (
+                                        <img
+                                            src={`${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/${user.cover_image}`}
+                                            alt="Cover"
+                                            className="w-full h-24 object-cover rounded-t-lg"
+                                        />
+                                    ) : (
+                                        <div
+                                            className="w-full h-24 rounded-t-lg"
+                                            style={{ backgroundColor: '#D1D5DB' }}
+                                        ></div>
+                                    )}
                                     <img
-                                        src={`${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/${user.cover_image}`}
-                                        alt="Cover"
-                                        className="w-full h-24 object-cover rounded-t-lg"
+                                        src={user.profile_image ? `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/${user.profile_image}` : "/profile.png"}
+                                        alt="Profile"
+                                        className="w-12 h-12 object-cover rounded-full absolute top-16 left-4 border-2 border-white"
                                     />
-                                ) : (
-                                    <div
-                                        className="w-full h-24 rounded-t-lg"
-                                        style={{ backgroundColor: '#D1D5DB' }}
-                                    ></div>
-                                )}
-                                <img
-                                    src={user.profile_image ? `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/${user.profile_image}` : "/profile.png"}
-                                    alt="Profile"
-                                    className="w-12 h-12 object-cover rounded-full absolute top-16 left-4 border-2 border-white"
-                                />
-                            </div>
-                            <div className="mt-8 flex justify-between items-center">
-                                <div>
-                                    <h2 className="text-lg font-bold">{user.name || "Display Name"}</h2>
-                                    <div className="flex items-center gap-2">
-                                        <p className="text-sm text-gray-500">Visitor</p>
-                                        <img src="/verified.png" className="h-6 w-6" alt="Verified" />
+                                </div>
+                                <div className="mt-8 flex justify-between items-center">
+                                    <div>
+                                        <h2 className="text-lg font-bold">{user.name || "Display Name"}</h2>
+                                        <div className="flex items-center gap-2">
+                                            <p className="text-sm text-gray-500">Visitor</p>
+                                            <img src="/verified.png" className="h-6 w-6" alt="Verified" />
+                                        </div>
+                                    </div>
+                                    <div className="text-right">
+                                        <p className="text-sm text-gray-500">X Followers</p>
+                                        <button className="mt-2 bg-gray-200 text-gray-700 font-semibold py-1 px-3 rounded-full flex items-center">
+                                            <span className="mr-2">+</span>Follow
+                                        </button>
                                     </div>
                                 </div>
-                                <div className="text-right">
-                                    <p className="text-sm text-gray-500">X Followers</p>
-                                    <button className="mt-2 bg-gray-200 text-gray-700 font-semibold py-1 px-3 rounded-full flex items-center">
-                                        <span className="mr-2">+</span>Follow
-                                    </button>
-                                </div>
                             </div>
-
-                        </div>
+                        </Link>
                     ))}
                 </div>
             </div>
