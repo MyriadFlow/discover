5b468981cf89b1cec44a9b8e4aa0e80f3b21251b
Suraj Mahto <surajmahto255@gmail.com>
2024-10-03 07:19:46 +0530

    feat : added fantoken in user assets

--- src/app/[username]/page.js
+++ src/app/[username]/page.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { useAccount, useChainId } from 'wagmi';
 import Moralis from 'moralis';
 import ProfileNftCard from '../../components/profileNftCard';
+import TokenNftCard from '../../components/tokenNftCard';
 import Header1 from '../../components/header1';
 import Footer from '../../components/footer';
 import Link from 'next/link';
@@ -17,6 +18,7 @@ function HomePage({ params }) {
 
   const [mintedNFTs, setMintedNFTs] = useState([]);
   const [matchedNFTs, setMatchedNFTs] = useState([]);
+  const [matchedWebxrs, setMatchedWebxr] = useState([]);
   const [name, setName] = useState('');
   const [coverImage, setCoverImage] = useState('');
   const [profileImage, setProfileImage] = useState('');
@@ -152,44 +154,98 @@ function HomePage({ params }) {
         const matched = await Promise.all(phyresult.map(async (phygital) => {
           const amountBought = await mintedNFTs.reduce(async (countPromise, nft) => {
             const count = await countPromise;
-            if (nft?.contract_type === 'ERC1155') {
-              try {
-                const response = await fetch(`${baseUri}/get-mint-fantoken/${address}`, {
-                  method: 'GET',
-                  headers: {
-                    'Content-Type': 'application/json'
-                  }
-                });
-                
-                if (!response.ok) {
-                  throw new Error('Failed to fetch minted tokens');
-                }
-                
-                const mintedTokens = await response.json();
-                
-                const matchingToken = mintedTokens.find(token => {
-                  return token.nftContractAddress === phygital.contract_address && 
-                         token.token_id === nft.token_id;
-                });
-                return count + (matchingToken ? 1 : 0);
-              } catch (error) {
-                console.error('Error fetching minted tokens:', error);
-                return count;
-              }
-            } else {
-              // For non-ERC1155 tokens, use the existing logic
-              return count + (nft?.token_address === phygital.contract_address ? 1 : 0);
-            }
+            return count + (nft?.token_address === phygital.contract_address ? 1 : 0);
           }, Promise.resolve(0));
           return {
             ...phygital,
             amount_bought: amountBought
           };
         }));
-        
+
         const filteredMatched = matched.filter(phygital => phygital.amount_bought > 0);
         setMatchedNFTs(filteredMatched);
         console.log("Matched NFTs:", filteredMatched);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    if (mintedNFTs.length > 0) {
+      fetchPhygitals();
+    }
+  }, [mintedNFTs, baseUri]);
+
+  useEffect(() => {
+    const fetchPhygitals = async () => {
+      try {
+        const phyres = await fetch(`${baseUri}/phygitals/all/554b4903-9a06-4031-98f4-48276c427f78`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+
+        if (!phyres.ok) {
+          throw new Error('Failed to fetch data');
+        }
+
+        const phyresult = await phyres.json();
+        console.log("Phygitals:", phyresult);
+
+        const matchedfantoken = await Promise.all(phyresult.map(async (phygital) => {
+          const amountBought = await mintedNFTs.reduce(async (countPromise, nft) => {
+            const count = await countPromise;
+            if (nft?.contract_type === 'ERC1155') {
+              try {
+                const response = await fetch(`${baseUri}/get-mint-fantoken/${walletAddress}`, {
+                  method: 'GET',
+                  headers: {
+                    'Content-Type': 'application/json'
+                  }
+                });
+        
+                if (!response.ok) {
+                  throw new Error('Failed to fetch minted tokens');
+                }
+        
+                const mintedFanTokens = await response.json();
+        
+                const matchingFanToken = mintedFanTokens.find(token => {
+                  return token.nftContractAddress === phygital.contract_address &&
+                    token.token_id === nft.token_id;
+                });
+                return count + (matchingFanToken ? 1 : 0);
+              } catch (error) {
+                console.error('Error fetching minted tokens:', error);
+                return count;
+              }
+            }
+            return count; 
+          }, Promise.resolve(0));
+          return {
+            ...phygital,
+            amount_bought: amountBought
+          };
+        }));
+        const filteredMatchedFanToken = matchedfantoken.filter(phygital => phygital.amount_bought > 0);
+        const response = await fetch(`${baseUri}/webxr/all/554b4903-9a06-4031-98f4-48276c427f78`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch minted tokens');
+        }
+
+        const webxr = await response.json();
+        const matchingWebxr = webxr.filter(web =>
+          filteredMatchedFanToken.some(phygital => phygital.id === web.phygital_id)
+        );
+        setMatchedWebxr(matchingWebxr);
+        console.log("Matched Fantoken:", filteredMatchedFanToken);
+        console.log("Matched webxr:", matchingWebxr);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -463,11 +519,18 @@ function HomePage({ params }) {
 
           <div style={{ padding: '50px' }}>
             {activeSection === 'assets' && (
-              <div className='mt-10 flex' style={{ gap: '20px', flexWrap: 'wrap' }}>
-                {matchedNFTs?.map((nft, index) => (
-                  <ProfileNftCard key={index} nft={nft} />
-                ))}
-              </div>
+              <>
+                <div className='mt-10 flex' style={{ gap: '20px', flexWrap: 'wrap' }}>
+                  {matchedNFTs?.map((nft, index) => (
+                    <ProfileNftCard key={index} nft={nft} />
+                  ))}
+                </div>
+                <div className='mt-10 flex' style={{ gap: '20px', flexWrap: 'wrap' }}>
+                  {Array.isArray(matchedWebxrs) && matchedWebxrs.map((nft, index) => (
+                    <TokenNftCard key={index} nft={nft} />
+                  ))}
+                </div>
+              </>
             )}
 
             {activeSection !== 'assets' && activeSection !== 'create' && (
@@ -651,11 +714,18 @@ function HomePage({ params }) {
 
           <div style={{ padding: '50px' }}>
             {activeSection === 'assets' && (
-              <div className='mt-10 flex' style={{ gap: '20px', flexWrap: 'wrap' }}>
-                {matchedNFTs?.map((nft, index) => (
-                  <ProfileNftCard key={index} nft={nft} />
-                ))}
-              </div>
+              <>
+                <div className='mt-10 flex' style={{ gap: '20px', flexWrap: 'wrap' }}>
+                  {matchedNFTs?.map((nft, index) => (
+                    <ProfileNftCard key={index} nft={nft} />
+                  ))}
+                </div>
+                <div className='mt-10 flex' style={{ gap: '20px', flexWrap: 'wrap' }}>
+                  {Array.isArray(matchedWebxrs) && matchedWebxrs.map((nft, index) => (
+                    <TokenNftCard key={index} nft={nft} />
+                  ))}
+                </div>
+              </>
             )}
 
             {activeSection !== 'assets' && activeSection !== 'create' && (
