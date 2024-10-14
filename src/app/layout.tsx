aaa5a3eabe12439eebb2c6128b28cf2560859412
Suraj Mahto <surajmahto255@gmail.com>
2024-10-15 00:11:24 +0530

    feat : updated metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,34 +21,29 @@ export const metadata: Metadata = {
 	title: 'Discover | MyriadFlow',
 	description: 'Own the future of collecting! MyriadFlow Discover lets you buy, sell, and showcase unique phygital NFTs. Explore immersive VR experiences that bring your digital collectibles to life.',
 	openGraph: {
-		type: 'website',
-		url: 'https://discover.myriadflow.com',
-		title: 'Discover | MyriadFlow',
-		description: 'Own the future of collecting! MyriadFlow Discover lets you buy, sell, and showcase unique phygital NFTs. Explore immersive VR experiences that bring your digital collectibles to life.',
-		images: [
-			{
-				url: '/discover_share_image_web.png',
-				width: 1200,
-				height: 630,
-				alt: 'MyriadFlow Discover',
-			},
-		],
+	  type: 'website',
+	  url: 'https://discover.myriadflow.com',
+	  title: 'Discover | MyriadFlow',
+	  description: 'Own the future of collecting! MyriadFlow Discover lets you buy, sell, and showcase unique phygital NFTs. Explore immersive VR experiences that bring your digital collectibles to life.',
+	  images: [
+		{
+		  url: 'https://opengraph.b-cdn.net/production/images/b8d63114-1c81-47e6-810d-740b32eef545.jpg?token=s6gIm_7zSPm9TQ972FNZdF14HcTS7Nhk0XPSLOr2ut0&height=750&width=1200&expires=33264930918',
+		  width: 1200,
+		  height: 750,
+		  alt: 'MyriadFlow Discover',
+		},
+	  ],
 	},
 	twitter: {
-		card: 'summary_large_image',
-		site: '@MyriadFlow',
-		title: 'Discover | MyriadFlow',
-		description: 'Own the future of collecting! MyriadFlow Discover lets you buy, sell, and showcase unique phygital NFTs. Explore immersive VR experiences that bring your digital collectibles to life.',
-		images: [
-			{
-				url: '/discover_share_image_web.png',
-				width: 1200,
-				height: 630,
-				alt: 'MyriadFlow Discover',
-			},
-		],
+	  card: 'summary_large_image',
+	  site: '@MyriadFlow',
+	  title: 'Discover | MyriadFlow',
+	  description: 'Own the future of collecting! MyriadFlow Discover lets you buy, sell, and showcase unique phygital NFTs. Explore immersive VR experiences that bring your digital collectibles to life.',
+	  images: [
+		'https://opengraph.b-cdn.net/production/images/b8d63114-1c81-47e6-810d-740b32eef545.jpg?token=s6gIm_7zSPm9TQ972FNZdF14HcTS7Nhk0XPSLOr2ut0&height=750&width=1200&expires=33264930918',
+	  ],
 	},
-}
+  }
 
 export default function RootLayout({
 	children,
