3f9e68fe57ec4b92e5e498619f5ca7fcf1dcb76f
Suraj Mahto <surajmahto255@gmail.com>
2024-11-02 00:33:53 +0530

    feat : updated leaderboard

---
+++ src/utils/queries.ts
@@ -0,0 +1,51 @@
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+
+export const baseURI =
+	process.env.NEXT_PUBLIC_URI || 'https://app.myriadflow.com'
+
+export const getBrands = async () => {
+	const response = await axios.get(`${baseURI}/brands/all`)
+	return response.data
+}
+
+export const getCollections = async () => {
+	const response = await axios.get(`${baseURI}/collections/all`)
+	return response.data
+}
+
+export const getPhygitals = async () => {
+	const response = await axios.get(`${baseURI}/phygitals/all`)
+	return response.data
+}
+
+export const getPhygital = async (id: string) => {
+	const response = await axios.get(`${baseURI}/phygitals/${id}`)
+	return response.data
+}
+
+export const getWebXR = async (id: string) => {
+	const response = await axios.get(`${baseURI}/webxr/phygital/${id}`)
+	return response.data
+}
+
+export const getAvatars = async () => {
+	const response = await axios.get(`${baseURI}/avatars/all`)
+	return response.data
+}
+
+export const getFanTokens = async () => {
+	const response = await axios.get(`${baseURI}/fantoken/all`)
+	return response.data
+}
+
+export const getFanMainTokens = async () => {
+	const response = await axios.get(`${baseURI}/get-mint-fantoken`)
+	return response.data
+}
+
+export const getProfileByWallet = async (walletAddress: string) => {
+	const response = await axios.get(`${baseURI}/profile/wallet/${walletAddress}`)
+
+	return response
+}
